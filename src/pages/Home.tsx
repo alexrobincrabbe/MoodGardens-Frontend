6eb877fa370ef90df474693ed97089811d5f1a1c
alexrobincrabbe <157045573+alexrobincrabbe@users.noreply.github.com>
2025-11-21 18:04:25 +0100

    home page layout fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ export function Home() {
   const { user, authReady } = useAuthData();
  
   return (
-    <div className="m-auto flex h-[80vh] w-fit flex-col items-center justify-center rounded-4xl bg-white md:p-10">
+    <div className="m-auto flex min-h-[80vh] w-fit flex-col items-center justify-center rounded-4xl bg-white md:p-10">
       <h1 className="text-plant-green mt-4 text-center text-2xl font-semibold md:hidden">
         Mood Gardens
       </h1>
